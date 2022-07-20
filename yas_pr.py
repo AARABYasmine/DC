4e2918cd40777f473ea542994bb9f5cd5e8d67f2
AARABYasmine <yasmineaarab2017@gmail.com>
2022-07-20 18:23:18 +0200

    second

--- yas_pr.py
+++ yas_pr.py
@@ -6,39 +6,43 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import Select
 import json
 import time
+from scrapy import Selector
 
 PATH="C:\Program Files (x86)\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
 driver.get("https://realpython.github.io/fake-jobs/")
+response=Selector(text=driver.page_source)
+elem=response.xpath('//*[@id="ResultsContainer"]')
 Data_liste=[]
-elem=driver.find_elements(By.ID, "ResultsContainer")
-for elems in elem:
-    i = 1
-    while i<=5:
+for i in range(1,3):
     #try:
     #  job_title1 = WebDriverWait(driver, 5).until(
     #        EC.presence_of_element_located((By.XPATH, '//h2'))
     #   )
       #driver.switch_to.frame(driver.find_element(By.XPATH,'//*[@id="ResultsContainer"]/div[i]/div/div/div[1]/div[2]/h2')).text
-      job_title1=elems.find_element(By.XPATH, '//*[@id="ResultsContainer"]/div["+i+"]/div/div/div[1]/div[2]/h2').text
-      name1=elems.find_element(By.XPATH, '//*[@id="ResultsContainer"]/div["i+1"]/div/div/div[1]/div[2]/h3').text
-      city1=elems.find_element(By.XPATH, '//*[@id="ResultsContainer"]/div["i+1"]/div/div/div[2]/p[1]').text
-      Date1=elems.find_element(By.XPATH, '//*[@id="ResultsContainer"]/div["i+1"]/div/div/div[2]/p[2]').text
+      job_title1=response.xpath('//div['+str(i)+']//h2/text()').get()
+      name1=response.xpath ('//div['+str(i)+']//h3/text()').get()
+      city1=response.xpath ('//div['+str(i)+']/div/div/div[2]/p[1]/text()').get().strip()
+      Date1=response.xpath ('//div['+str(i)+']/div/div/div[2]/p[2]/time/text()').get()
+      driver.find_element("xpath",'//div['+str(i)+']//a[2]').click()
+      #description1 = WebDriverWait(driver, 5).until(
+      #      EC.presence_of_element_located((By.XPATH, '//p[1]/text()'))
+      # )
       #driver.switch_to.frame(driver.find_element(By.XPATH,'//*[@id="ResultsContainer"]/div[2]'))
       #text_apply1=elem.find_element(By.LINK_TEXT, "Apply").click()
-      i=i+1
       dictionary ={
            'job_title':job_title1,
             'name' :name1,
             'city' :city1,
-           'Date' :Date1
-           #'text_apply': text_apply1
+           'Date' :Date1,
+       #    'description': description1
       }
       Data_liste.append(dictionary)
-      with open("DC.json", "w") as outfile:
-       json.dump(Data_liste, outfile)
+      time.sleep(3)
+with open("DC.json", "w") as outfile:
+    json.dump(Data_liste, outfile)
 #      Data_liste.append(job_title1)
 
 #    finally:
-    driver.quit()
+    #driver.quit()
 #print(job_title1)
